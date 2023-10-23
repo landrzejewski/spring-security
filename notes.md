0c9ded9da9b3400a84b40403bdf0bb5628bcb7af
landrzejewski <landrzejewski.poczta@gmail.com>
2023-10-23 10:41:35 +0200

    base

---
+++ notes.md
@@ -0,0 +1,2 @@
+Basic Authorization header generator
+https://www.blitter.se/utils/basic-authentication-header-generator
---
+++ src/main/java/pl/training/shop/security/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+package pl.training.shop.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@Configuration
+public class SecurityConfiguration {
+
+    /*AuthenticationManager authenticationManager; // Interfejs/kontrakt dla procesu uwierzytelnienia użytkownika
+        ProviderManager providerManager; // Podstawowa implementacja AuthenticationManager, deleguje proces uwierzytelnienia do jednego z obiektów AuthenticationProvider
+            AuthenticationProvider authenticationProvider; // Interfejs/kontrakt dla obiektów realizujących uwierzytelnianie z wykorzystaniem konkretnego mechanizmu/implementacji
+                DaoAuthenticationProvider daoAuthenticationProvider; // Jedna z implementacji AuthenticationProvider, ładuje dane o użytkowniku wykorzystując UserDetailsService i porównuje je z tymi podanymi w czasie logowani
+                    UserDetailsService userDetailsService; // Interfejs/kontrakt usługi ładującej dane dotyczące użytkownika
+
+    UsersDetailsManager usersDetailsManager; Interfejs/kontrakt pochodny UserDetailsService, pozwalający na zarządzanie użytkownikami
+        InMemoryUserDetailsManager inMemoryUserDetailsManager; // Jedna z implementacji UsersDetailsManager, przechowuje informacje w pamięci
+
+    PasswordEncoder passwordEncoder; //Interfejs/kontrakt pozwalający na hashowanie i porównywanie haseł
+        BCryptPasswordEncoder bCryptPasswordEncoder; //Jedna z implementacji PasswordEncoder
+
+    SecurityContextHolder securityContextHolder; // Przechowuje/udostępnia SecurityContext
+        SecurityContext securityContext; // Kontener przechowujący Authentication
+            Authentication authentication; // Reprezentuje dane uwierzytelniające jak i uwierzytelnionego użytkownika/system
+                UserDetails userDetails; // Interfejs/kontrakt opisujący użytkownika
+                GrantedAuthority grantedAuthority; // Interfejs/kontrakt opisujący role/uprawnienia
+                    SimpleGrantedAuthority simpleGrantedAuthority; // Jedna z implementacji SimpleGrantedAuthority
+
+    AuthorizationManager authorizationManager; // Interfejs/kontrakt dla procesu autoryzacji
+        AuthoritiesAuthorizationManager authoritiesAuthorizationManager; // Jedna z implementacji AuthorizationManager (role)*/
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance(); //deprecated
+    }
+
+    /*@Bean
+    public UserDetailsManager userDetailsManager() {
+        var user = User
+                .withUsername("admin")
+                .password("admin")
+                .roles("ADMIN")
+                //.authorities("create", "read", "delete")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }*/
+
+}
