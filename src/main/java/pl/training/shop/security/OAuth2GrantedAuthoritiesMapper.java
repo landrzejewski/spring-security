0b594dc8fd02e1c1848193fed0f5a22137fe17ef
landrzejewski <landrzejewski.poczta@gmail.com>
2023-10-24 16:29:32 +0200

    base

---
+++ src/main/java/pl/training/shop/security/OAuth2GrantedAuthoritiesMapper.java
@@ -0,0 +1,43 @@
+package pl.training.shop.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class OAuth2GrantedAuthoritiesMapper {
+
+    private static final String REALM_CLAIM = "realm_access";
+    private static final String ROLES_CLAIM = "roles";
+    private static final String ROLE_PREFIX = "ROLE_";
+
+    @SuppressWarnings("unchecked")
+    public Collection<GrantedAuthority> map(Collection<? extends GrantedAuthority> authorities) {
+        var grantedAuthorities = new HashSet<String>();
+        authorities.forEach(authority -> {
+           /* if (authority instanceof OidcUserAuthority oidcUserAuthority) {
+                var userInfo = oidcUserAuthority.getUserInfo();
+                var realmAccess = userInfo.getClaimAsMap(REALM_CLAIM);
+                var roles = (Collection<String>) realmAccess.get(ROLES_CLAIM);
+                grantedAuthorities.addAll(roles);
+            } else */
+            if (authority instanceof OAuth2UserAuthority oauth2UserAuthority) {
+                var userAttributes = oauth2UserAuthority.getAttributes();
+                var realmAccess = (Map<String, Object>) userAttributes.get(REALM_CLAIM);
+                var roles = (Collection<String>) realmAccess.get(ROLES_CLAIM);
+                grantedAuthorities.addAll(roles);
+            } else {
+                grantedAuthorities.add(authority.getAuthority());
+            }
+        });
+        return grantedAuthorities.stream()
+                .map(role -> ROLE_PREFIX + role)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+    }
+
+}
--- src/main/java/pl/training/shop/security/SecurityConfiguration.java
+++ src/main/java/pl/training/shop/security/SecurityConfiguration.java
@@ -5,18 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer.UserInfoEndpointConfig;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
-import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Configuration
 public class SecurityConfiguration {
@@ -51,35 +43,9 @@ public class SecurityConfiguration {
         jwtConfigurer.jwtAuthenticationConverter(converter);
     }
 
-    private static final String REALM_CLAIM = "realm_access";
-    private static final String ROLES_CLAIM = "roles";
-    private static final String ROLE_PREFIX = "ROLE_";
-
     // Client scopes -> Client scope details (roles) -> Mapper details -> Add to userinfo enabled (Keycloak Admin console)
-    @SuppressWarnings("unchecked")
     private void oauth2LoginConfig(UserInfoEndpointConfig config) {
-        config.userAuthoritiesMapper(authorities -> {
-            var grantedAuthorities = new HashSet<String>();
-            authorities.forEach(authority -> {
-                if (authority instanceof OidcUserAuthority oidcUserAuthority) {
-                    var userInfo = oidcUserAuthority.getUserInfo();
-                    var realmAccess = userInfo.getClaimAsMap(REALM_CLAIM);
-                    var roles = (Collection<String>) realmAccess.get(ROLES_CLAIM);
-                    grantedAuthorities.addAll(roles);
-                } else if (authority instanceof OAuth2UserAuthority oauth2UserAuthority) {
-                    var userAttributes = oauth2UserAuthority.getAttributes();
-                    var realmAccess = (Map<String, Object>) userAttributes.get(REALM_CLAIM);
-                    var roles = (Collection<String>) realmAccess.get(ROLES_CLAIM);
-                    grantedAuthorities.addAll(roles);
-                } else {
-                    grantedAuthorities.add(authority.getAuthority());
-                }
-            });
-            return grantedAuthorities.stream()
-                    .map(role -> ROLE_PREFIX + role)
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toSet());
-        });
+        config.userAuthoritiesMapper(authorities -> new OAuth2GrantedAuthoritiesMapper().map(authorities));
     }
 
 }
