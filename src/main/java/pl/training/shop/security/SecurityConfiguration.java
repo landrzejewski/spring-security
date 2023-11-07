282f4485532b1aeb34dac61904001459b1f28f87
landrzejewski <landrzejewski.poczta@gmail.com>
2023-11-07 13:02:00 +0100

    x

--- src/main/java/pl/training/shop/security/SecurityConfiguration.java
+++ src/main/java/pl/training/shop/security/SecurityConfiguration.java
@@ -3,9 +3,13 @@ package pl.training.shop.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class SecurityConfiguration {
@@ -16,11 +20,19 @@ public class SecurityConfiguration {
                 .csrf(config -> config
                         .ignoringRequestMatchers("/api/**")
                 )
+                .oauth2Login(config -> config
+                        .userInfoEndpoint(this::userInfoEndpointConfigurer)
+                )
                 .oauth2ResourceServer(config -> config
                         .jwt(this::jwtConfigurer)
                 )
                 .authorizeHttpRequests(config -> config
                         .anyRequest().hasRole("ADMIN")
+                )
+                .logout(config -> config
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout.html"))
+                        .addLogoutHandler(new KeycloakLogoutHandler(new RestTemplate()))
+                        .logoutSuccessUrl("/index.html")
                 )
                 .build();
     }
@@ -29,6 +41,11 @@ public class SecurityConfiguration {
         var jwtConverter = new JwtAuthenticationConverter();
         jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtGrantedAuthoritiesConverter());
         jwtConfigurer.jwtAuthenticationConverter(jwtConverter);
+    }
+
+    // Client scopes -> Client scope details (roles) -> Mapper details -> Add to userinfo enabled (Keycloak Admin console)
+    private void userInfoEndpointConfigurer(OAuth2LoginConfigurer.UserInfoEndpointConfig userInfoEndpointConfig) {
+        userInfoEndpointConfig.userAuthoritiesMapper(new KeycloakGrantedAuthoritiesMapper());
     }
 
 }
