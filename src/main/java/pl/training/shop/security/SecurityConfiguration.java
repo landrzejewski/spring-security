cd364205a6992aedaf5109654be151f370e70bd3
landrzejewski <landrzejewski.poczta@gmail.com>
2023-11-14 14:03:30 +0100

    x

--- src/main/java/pl/training/shop/security/SecurityConfiguration.java
+++ src/main/java/pl/training/shop/security/SecurityConfiguration.java
@@ -3,9 +3,12 @@ package pl.training.shop.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.IF_REQUIRED;
 
 @Configuration
 public class SecurityConfiguration {
@@ -19,10 +22,18 @@ public class SecurityConfiguration {
                 .oauth2ResourceServer(config -> config
                         .jwt(this::jwtConfigurer)
                 )
+                .oauth2Login(config -> config.userInfoEndpoint(this::userInfoCustomizer))
                 .authorizeHttpRequests(config -> config
-                        .anyRequest().hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(config -> config
+                        .sessionCreationPolicy(IF_REQUIRED)
                 )
                 .build();
+    }
+
+    private void userInfoCustomizer(OAuth2LoginConfigurer.UserInfoEndpointConfig userInfoEndpointConfig) {
+        userInfoEndpointConfig.userAuthoritiesMapper(new KeycloakGrantedAuthoritiesMapper());
     }
 
     private void jwtConfigurer(OAuth2ResourceServerConfigurer.JwtConfigurer jwtConfigurer) {
